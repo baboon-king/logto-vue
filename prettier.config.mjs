a0dc654dfa519f4799c3aea384cb1cb90815c62d
BaboonKing <changfan.xu.up@qq.com>
2024-04-30 14:34:07 +0800

    chore: prettier setup

---
+++ prettier.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+/** @typedef { import("@ianvs/prettier-plugin-sort-imports").PluginConfig } SortImportsConfig */
+/** @typedef { import("prettier").Config } PrettierConfig */
+
+/** @type { PrettierConfig | SortImportsConfig } */
+export default {
+  printWidth: 100,
+  singleAttributePerLine: true,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.0.0",
+  importOrder: [
+    "<TYPES>^(bun:)",
+    "<TYPES>^(node:)",
+    "<TYPES>",
+    "",
+    "<BUILT_IN_MODULES>",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^[.|..]",
+    "",
+    "^[../]",
+    "^[./]",
+  ],
+};
